ad53ed6f4b613ca18e575ee009281d9f7c6a46e7
Juan Collado <juancollado@protonmail.com>
2022-05-30 20:35:13 -0300

    Using Faker

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,27 @@
-import { addProduct } from './products/product.service';
+import { faker } from '@faker-js/faker';
+import { addProduct, products } from './products/product.service';
 
-addProduct({
-    id: '1',
-    title: 'p1',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-    stock: 90,
-    category: {
-        id: '12',
-        name: 'c1',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-    },
-});
+for (let i = 0; i < 50; i++) {
+    addProduct({
+        id: faker.datatype.uuid(),
+        description: faker.commerce.productDescription(),
+        title: faker.commerce.productName(),
+        createdAt: faker.date.recent(),
+        updatedAt: faker.date.recent(),
+        stock: faker.datatype.number({ min: 10, max: 10000 }),
+        category: {
+            id: faker.datatype.uuid(),
+            name: faker.commerce.department(),
+            createdAt: faker.date.recent(),
+            updatedAt: faker.date.recent(),
+        },
+        image: faker.image.imageUrl(),
+        color: faker.color.rgb(),
+        price: parseInt(faker.commerce.price(), 10),
+        isNew: faker.datatype.boolean(),
+        tags: faker.helpers.arrayElements(),
+        size: faker.helpers.arrayElement(['S', 'M', 'L']),
+    });
+}
+
+console.log('PRODUCTOS', products);
