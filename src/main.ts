984cdce0e352149e81f829f0ab720e9a267bd13a
Juan Collado <juancollado@protonmail.com>
2022-06-06 20:48:06 -0300

    DTO to create products

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,10 @@ import { addProduct, products } from './products/product.service';
 
 for (let i = 0; i < 50; i++) {
     addProduct({
-        id: faker.datatype.uuid(),
         description: faker.commerce.productDescription(),
         title: faker.commerce.productName(),
-        createdAt: faker.date.recent(),
-        updatedAt: faker.date.recent(),
         stock: faker.datatype.number({ min: 10, max: 10000 }),
-        category: {
-            id: faker.datatype.uuid(),
-            name: faker.commerce.department(),
-            createdAt: faker.date.recent(),
-            updatedAt: faker.date.recent(),
-        },
+        categoryId: faker.datatype.uuid(),
         image: faker.image.imageUrl(),
         color: faker.color.rgb(),
         price: parseInt(faker.commerce.price(), 10),
--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -1,7 +1,23 @@
+import { faker } from '@faker-js/faker';
+import { CreateProductDto } from './product.dto';
 import { Product } from './product.model';
 
 export const products: Product[] = [];
 
-export const addProduct = (data: Product) => {
-  products.push(data);
-}
+export const addProduct = (data: CreateProductDto): Product => {
+    const NOW = new Date();
+    const newProduct: Product = {
+        ...data,
+        createdAt: NOW,
+        updatedAt: NOW,
+        id: faker.datatype.uuid(),
+        category: {
+            id: data.categoryId,
+            createdAt: NOW,
+            updatedAt: NOW,
+            name: faker.commerce.department(),
+        },
+    };
+    products.push(newProduct);
+    return newProduct;
+};
